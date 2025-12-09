b7ff33819dd1d5a5e072b3e45314626fa1504116
Nimesh Dulal <nimeshdulal870@gmail.com>
2025-12-09 20:52:17 +0545

    Completed portfolio design

--- script.js
+++ script.js
@@ -28,6 +28,14 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
             });
+
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+
+        // if want to use Animation that repeats on scroll use this
+        else{
+            sec.classList.remove('show-animate');
         }
     });
 
